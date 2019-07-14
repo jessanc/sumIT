ebaef29625f8daf1266c663b206c676059901c3b
Jessan <jessan.cherian@gmail.com>
2019-07-13 19:52:02 -0700

    not working

--- handler.js
+++ handler.js
@@ -3,7 +3,8 @@
 const AWS = require('aws-sdk');
 const uuid = require('uuid/v4');
 const fileType = require('file-type');
-
+const rp = require('request-promise');
+const request = require('request');
 AWS.config.setPromisesDependency(require('bluebird'));
 
 const s3BasePath = 'https://sumitaudios.s3-us-west-2.amazonaws.com/';
@@ -22,7 +23,7 @@ module.exports.uploadAudio = (event, context, callback) => {
 
   let fileMime = fileType(buffer);
 
-  if(fileMime === null){
+  if (fileMime === null) {
     return context.fail("file type is not supported");
   }
 
@@ -38,17 +39,17 @@ module.exports.uploadAudio = (event, context, callback) => {
     Bucket: 'sumitaudios',
     Key: fileName,
     Body: buffer,
-    ContentType : 'audio/wav',
+    ContentType: 'audio/wav',
     ACL: 'public-read'
   };
 
-  s3.putObject(s3Params).promise().then(()=>{
+  s3.putObject(s3Params).promise().then(() => {
     console.log("Uploaded to " + fileFullPath);
-  }).then((res)=> {
+  }).then((res) => {
     let audioRecord = {
-      MeetingId : meetingID,
-      Email : email,
-      s3AudioFile : fileFullPath
+      MeetingId: meetingID,
+      Email: email,
+      s3AudioFile: fileFullPath
     }
     postToDDB(audioRecord).then(res => {
       callback(null, {
@@ -63,7 +64,7 @@ module.exports.uploadAudio = (event, context, callback) => {
 };
 
 let postToDDB = audioRecord => {
-  console.log('Submitting audioRecord'+ JSON.stringify(audioRecord));
+  console.log('Submitting audioRecord' + JSON.stringify(audioRecord));
   console.log("table: " + process.env.ddbTable);
   const audioInfo = {
     TableName: process.env.ddbTable,
@@ -71,6 +72,84 @@ let postToDDB = audioRecord => {
   };
   return dynamoDb.put(audioInfo).promise()
     .then(res => audioInfo);
+}
+
+module.exports.summarize = async (event, context, callback) => {
+  console.log('inside summarize');
+  var json = JSON.parse(event.body);
+  console.log('json: ' + json);
+  var s3transcribedUrl = json.transcribeUrl;
+
+  console.log("transcribeUrl: " + s3transcribedUrl);
+  if (s3transcribedUrl === null) {
+    console.log('No s3 url passed');
+    context.fail("No s3 url passed");
+  }
+
+  var headers = {
+    'User-Agent': 'Super Agent/0.0.1',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+
+  var options = {
+    uri: 'https://resoomer.pro/summarizer/',
+    method: 'POST',
+    headers: headers,
+    form: { 'API_KEY': '80F0D9B0265ABD0F98E33EB9316A578C', 'url': s3transcribedUrl }
+  }
+
+
+  var options2 = {
+    uri: s3transcribedUrl,
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true // Automatically parses the JSON string in the response
+  };
+
+  var text = '';
+
+  request.get(s3transcribedUrl, function (res, err) {
+    if (err) {
+      console.log(err);
+      callback(null, {
+        statusCode: 400,
+        headers: {
+          'Access-Control-Allow-Origin': '*' //probably only allow the AWS ec2 instance to access
+        },
+        body: JSON.stringify({ ERROR: err }),
+      })
+    }
+    else {
+      console.log('response : ' + res);
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*' //probably only allow the AWS ec2 instance to access
+        },
+        body: JSON.stringify({ summarizedS3Url: res }),
+      })
+    }
+  });
+  console.log("calling request");
+  // Start the request
+  // rp(options)
+  //   .then((parsedBody) => {
+  //     // POST succeeded...
+  //     console.log("resoomer call successful, response: " + parsedBody);
+  //     callback(null, {
+  //       statusCode: 200,
+  //       headers: {
+  //         'Access-Control-Allow-Origin': '*' //probably only allow the AWS ec2 instance to access
+  //       },
+  //       body: JSON.stringify({ summarizedS3Url: "fakeUrl" }),
+  //     })
+  //   })
+  //   .catch((err) => {
+  //     // POST failed...
+  //     console.log('resoomer call failed' + err);
+  //     context.fail('resoomer call failed' + err);
+  //   });
 }
 
 
